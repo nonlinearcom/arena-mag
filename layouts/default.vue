bcb6a6ac9ec8498ffe06ec9351764e9bf0c17d93
Manuel Ehrenfeld <manuel@non-linear.com>
2020-02-09 16:00:59 +0100

    homepage WIP

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,17 +5,19 @@
 </template>
 
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+html, body {
+	height: 100%;
 }
+body{
+    font-family: 'Inter', 'Helvetica Neue', Arial, sans-serif;
+	font-size: 21px;
+	line-height:  1.4;
+	font-weight: regular;
+    margin: 0;
+    -webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+}
+
 
 *,
 *:before,
@@ -24,32 +26,40 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+h1, h2, h3, h4, h5, h6,
+p, a {
+	font-size: 21px;
+	line-height:  1.4;
+	font-weight: regular;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+a {
+    color: #444444;
+    text-decoration: none;
+
+    &:hover{
+        color: #333333;
+    }
 }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
+.small-text{
+	font-size: var(--font-size-small);
 }
 
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+button:focus {
+    outline: none!important;
+    border: none;
+    background: none;
 }
+
+
+/* Page transitions */
+
+.page-enter-active, .page-leave-active {
+ 	transition: opacity .5s
+}
+.page-enter, .page-leave-active {
+  opacity: 0;
+}
+
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,68 @@
 <template>
-   <div>
+	<nav class="article__list">
 
-   </div>
+		<div v-html="about.content_html"></div>
+		<template v-for="(block, index) in channel">
+			<!-- slug or id -->
+			<nuxt-link :class="block.class" :key="index" :to="`/${block.slug}`">{{ block.title}}</nuxt-link>
+		</template>
+	</nav>
 </template>
 
 <script>
 
 export default {
-  components: {
-  }
-}
+
+	components: {
+	
+	},
+	data() {
+		return {
+			blocks: [],
+		}
+	},
+    async asyncData({ $axios, params, store }) {
+		//https://api.are.na/v2/channels/arena-mag?&direction=desc
+        const channel = await $axios
+			.$get(`https://api.are.na/v2/channels/${process.env.arenaChannel}?&direction=desc`)
+            .then(response => {
+
+				// get only channels
+                // return response.contents.filter(function(obj) {
+				// 	return obj.class == 'Channel';
+				// })
+
+				const articles = response.contents.filter(function(obj) {
+					return obj.class == 'Channel';
+				})
+
+				const about = response.contents.filter(function(obj) {
+					return obj.class == 'Text';
+				})
+
+				return {
+					articles,
+					about
+				}
+            })
+			.catch(e => console.log(e, "error"));
+			
+		
+
+		return {
+			// description: articles.metadata.description,
+			// channel: channel.contents
+			channel : channel.articles,
+			about: 	channel.about[0]
+        };
+	},
+};
 </script>
 
-<style>
+<style lang="css">
 
+.article__list{
+	font-size: 36px;
+	line-height: 1.4;
+}
 </style>
